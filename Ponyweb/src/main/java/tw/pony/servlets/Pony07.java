693049cf44cb692dfac2d7b9f732385cec1ffccb
pony40701 <pony40701@gmail.com>
2025-05-12 15:54:52 +0800

    05121554

--- Ponyweb/src/main/java/tw/pony/servlets/Pony07.java
+++ Ponyweb/src/main/java/tw/pony/servlets/Pony07.java
@@ -13,13 +13,23 @@ import com.sun.net.httpserver.Request;
 @WebServlet("/Pony07")
 public class Pony07 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
-		System.out.print("Pony:doGet\n");
+		doPost(request, response);
 	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
 		String account = req.getParameter("account");
 		String passwd = req.getParameter("passwd");
-		System.out.printf("Pony07:doPost:%s:%s\n",account,passwd);
+		String gender = req.getParameter("gender");
+		String[] habits = req.getParameterValues("Habits");
+		System.out.printf("%s",gender);
+		try {
+		for(String habit:habits) {
+			System.out.println(habit);
+		}
+		}catch(Exception e){
+			
+		}
+		System.out.printf("Pony07:doPost:%s:%s:%s:s\n",account,passwd,gender,habits);
 	}
 }
